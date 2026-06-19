25d91a8b7d32cb5892ae9dcc9f8d0a4a4862c519
khrisha29 <shkhrisha@gmail.com>
2026-06-19 19:04:34 +0530

    feat: assemble landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components/Navbar";
+import { HeroSection } from "@/components/HeroSection";
+import { TheSkyWeLost } from "@/components/TheSkyWeLost";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#020617]">
+      <Navbar />
+      <HeroSection />
+      <TheSkyWeLost />
+    </main>
+  );
+}
