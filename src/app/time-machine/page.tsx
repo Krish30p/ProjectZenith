ed6478c26a0621735bdedc4a214ab6f3b1b2683c
KrishnaPatel <kp06krish@gmail.com>
2026-06-26 00:19:01 +0530

    feat: add new time machine page route with CosmicTimeMachine component

---
+++ src/app/time-machine/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { CosmicTimeMachine } from "@/components/time-machine/CosmicTimeMachine";
+
+export default function TimeMachinePage() {
+  return (
+    <main className="relative min-h-screen bg-[#01030A]">
+      <CosmicTimeMachine />
+    </main>
+  );
+}
